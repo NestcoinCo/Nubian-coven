018110c41f97861f3c1a1d0f06f6d29f5154a247
nonseodion <nonseifebhor@gmail.com>
2021-10-28 17:31:26 +0100

    added test and connector abis

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,49 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const { utils } = require("ethers");
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+      },
+      {
+        version: "0.6.0",
+      },
+      {
+        version: "0.6.2",
+      },
+      {
+        version: "0.6.5",
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://bsc-dataseed.binance.org/`,
+      },
+      timeout: 200000,
+      blockGasLimit: 12000000,
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    bscmainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
--- test/swap.js
+++ test/swap.js
@@ -1,14 +1,62 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const basicABI = require("../abis/basic.json");
+const pancakeABI = require("../abis/pancake.json");
+
 
 describe("Nbn Wizard", async function () {
-  const [signer] = await ethers.getSigners();
-  const wizard = await ethers.getContractAt("NbnWizard", "", signer);
-  // connectors
-  const basic = await ethers.utils.Interface();
-  const pancake = await ethers.utils.Interface();
+  let IBasic, IPancake, busd;
+
+  before( async () => {
+    console.log("me");
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0x2EFb0e053321967b27147bf26CCeAAaB56022Da9"],
+    });
+    console.log("me")
+
+    const signer = await ethers.getSigner("0x2EFb0e053321967b27147bf26CCeAAaB56022Da9");
+    const wizard = await ethers.getContractAt("NbnWizard", process.env.WIZARD_ADDRESS, signer);
+    // connectors
+    IBasic = await new  ethers.utils.Interface(basicABI);
+    IPancake = await new ethers.utils.Interface(pancakeABI);
+    const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    const USDT = "0x55d398326f99059ff775485246999027b3197955";
+    // approve BUSD
+    busd = await ethers.getContractAt("IERC20", BUSD, signer);
+    await busd.approve(wizard.address, ethers.constants.MaxUint256);
+
+    const usdt = await ethers.getContractAt("IERC20", USDT, signer);
+    console.log("me");
+  }
+
+  )
 
   it("should swap assets", async function () {
-    
+    const deposit = IBasic.encodeFunctionData("deposit", [
+      BUSD,
+      ethers.utils.parseEther("1"),
+      0,
+      0,
+    ])
+    const withdraw = IBasic.encodeFunctionData("deposit", [
+      USDT,
+      ethers.constants.MaxUint256,
+      0,
+      0
+    ])
+    const sell = IPancake.encodeFunctionData("sell", [
+      USDT,
+      BUSD,
+      ethers.utils.parseEther("1"),
+      "2",
+      0,
+      0
+    ])
+
+    await expect(
+      () => wizard.cast(["Basic", "PancakeV2", "Basic"], [deposit, sell, withdraw], signer.getAddress())
+    ).to.changeTokenBalance(busd, signer,  ethers.utils.parseEther("1"));
   });
 });
